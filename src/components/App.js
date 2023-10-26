070598742a1688749d9ec3009341ecf1472d6c71
cany <couleurvide1008@gmail.com>
2023-10-26 16:49:16 +0900

    入力フォームに入力された文字列を新規TODOとして追加する機能を実装

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import React, { useRef } from "react";
+
 import { useTodo } from "../hooks/useTodo";
 
 // TodoTitleコンポーネント
@@ -39,10 +41,25 @@ const TodoList = ({ todoList }) => {
 };
 
 function App() {
-  // 現在のTODOの状態
-  const { todoList } = useTodo();
+  // カスタムフックで作成したtodoListとaddTodoListItemを利用
+  const { todoList, addTodoListItem } = useTodo();
 
-  console.log("TODOリスト:", todoList);
+  // useRefでrefオブジェクトを作成(TODO入力フォームで利用)
+  const inputEl = useRef(null);
+
+  // TODO乳力フォームで入力された文字列を新しいTODOに登録するための関数を宣言
+  const handleAddTodoListItem = () => {
+    // 何も入力されていない場合にクリックしても何も返さない
+    if (inputEl.current.value === "") return;
+
+    // テキストエリアに入力された文字列を新規TODOとして取得
+    // 追加したらテキストエリアを空にする
+    // 新規TODOを追加するaddTodoListItem関数をボタンクリックで実行
+    addTodoListItem(inputEl.current.value);
+    inputEl.current.value = "";
+  };
+
+  console.log("TODOリスト".todoList);
 
   // filterで「TODOの状態が未完了」の要素を持つ新しい配列を作成
   const inCompletedList = todoList.filter((todo) => {
@@ -61,8 +78,10 @@ function App() {
   return (
     <>
       <TodoTitle title="TODO進捗管理" as="h1" />
-      <textarea />
-      <button>TODOを追加</button>
+      {/* useRefで作成したRefオブジェクトをref属性に指定し、DOMを参照*/}
+      <textarea ref={inputEl} />
+      {/* ボタンをクリックするとhandleAddTodoListItem関数を実行*/}
+      <button onClick={handleAddTodoListItem}>TODOを追加</button>
       <TodoTitle title="未完了TODOリスト" as="h2" />
       <TodoList todoList={inCompletedList} />
       <TodoTitle title="完了TODOリスト" as="h2" />
